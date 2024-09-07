1a9a5f4412d3951e420f03aae4672c3ed3c8dfc0
Matt Rondel <mattrondel@gmail.com>
2024-09-07 16:24:57 -0700

    first

---
+++ card.py
@@ -0,0 +1,19 @@
+class Card:
+    def __init__(self, name, CMC,Type,Color,Set,Collector_Number,Rarity,Color_Category,status,Finish,maybeboard,image_URL,image_Back_URL,tags,Notes,MTGO_ID):
+        self.name = name
+        self.CMC = CMC
+        self.Type = Type
+        self.Color = Color
+        self.Set = Set
+        self.Collector_Number = Collector_Number
+        self.Rarity = Rarity
+        self.Color_Category = Color_Category
+        self.status = status
+        self.Finish = Finish
+        self.maybeboard = maybeboard
+        self.image_URL = image_URL
+        self.image_Back_URL = image_Back_URL
+        self.tags = tags
+        self.Notes = Notes
+        self.MTGO_ID = MTGO_ID
+
---
+++ main.py
@@ -0,0 +1,8 @@
+import card
+import csv
+
+csv_file = input()
+with open(csv_file, "r") as csv:
+    newcsv = csv.reader(csv)
+
+card = Card()
