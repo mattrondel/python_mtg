ef8bfd8f86a64f3af423693aa75c85dba7833a42
mattrondel <109989470+mattrondel@users.noreply.github.com>
2024-09-07 17:16:55 -0700

    .

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import card
 import csv
 
-csv_file = input()
-with open(csv_file, "r") as csv:
-    newcsv = csv.reader(csv)
+csv_file = "MishrazzPremodern360cube.csv" # input()
+cards = []
 
-card = card.Card()
+with open(csv_file, "r") as cube_file:
+    newcsv = csv.reader(cube_file)
+    next(newcsv) # Removes header
+    for each_card in newcsv:
+        # print(each_card)
+        print(each_card[0],each_card[2])
+        # cards.append(
+        #     card.Card(each_card[0])
+        # )
+
+
+#card = card.Card()
+
+# Moxfield syntax: QTY Name (set) collector
